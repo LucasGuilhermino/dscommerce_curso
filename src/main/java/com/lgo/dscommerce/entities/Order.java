a3391a578e960e0cbe7940fa396c559408fb5b0b
Lucas Guilhermino <lucasguilherminooliveira@gmail.com>
2026-04-29 12:30:35 -0300

    OrderItem e relacionamentos

--- src/main/java/com/lgo/dscommerce/entities/Order.java
+++ src/main/java/com/lgo/dscommerce/entities/Order.java
@@ -4,6 +4,9 @@ import com.lgo.dscommerce.entities.User;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -23,6 +26,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){}
 
@@ -72,5 +78,13 @@ public class Order {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts(){
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 }
