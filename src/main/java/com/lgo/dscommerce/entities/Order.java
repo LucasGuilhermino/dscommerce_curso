45c9b839c1b3de4e12a4b620f001a0fb2e6b39a3
Lucas Guilhermino <lucasguilherminooliveira@gmail.com>
2026-04-28 22:02:21 -0300

    First commit: User, banco h2, Order, OrderStatus e relacionamentos iniciais

---
+++ src/main/java/com/lgo/dscommerce/entities/Order.java
@@ -0,0 +1,24 @@
+package com.lgo.dscommerce.entities;
+
+import com.lgo.dscommerce.entities.User;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+}
